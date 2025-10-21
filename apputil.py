80ff8362f318009cca3ed10b9a44fd963b1a2591
Dmillz15 <160161559+Dmillz15@users.noreply.github.com>
2025-10-21 22:04:05 +0000

    Confused the parts but switched the exercise 1 to the apputil and finished the second exercise

--- apputil.py
+++ apputil.py
@@ -1,23 +1,37 @@
+import random
 from collections import defaultdict
 
-
-class MarkovText(object):
-
+class MarkovText:
     def __init__(self, corpus):
-        self.corpus = corpus
-        self.term_dict = None  # you'll need to build this
+        self.corpus = corpus.split()
+        self.term_dict = self.get_term_dict()
 
     def get_term_dict(self):
+        term_dict = defaultdict(list)
+        for i in range(len(self.corpus) - 1):
+            key = self.corpus[i]
+            next_word = self.corpus[i + 1]
+            term_dict[key].append(next_word)
+        return dict(term_dict)
 
-        # your code here ...
+    def generate(self, term_count=15, seed_term=None):
+        # Choose starting word
+        if seed_term:
+            if seed_term not in self.term_dict:
+                raise ValueError(f"Seed term '{seed_term}' not found in corpus.")
+            current_word = seed_term
+        else:
+            current_word = random.choice(list(self.term_dict.keys()))
 
-        self.term_dict = {}
+        generated = [current_word]
 
-        return None
+        # Generate the sequence
+        for _ in range(term_count - 1):
+            if current_word not in self.term_dict or not self.term_dict[current_word]:
+                break
+            next_word = random.choice(self.term_dict[current_word])
+            generated.append(next_word)
+            current_word = next_word
 
-
-    def generate(self, seed_term=None, term_count=15):
-
-        # your code here ...
-
-        return None
+        # ✅ Make sure you RETURN the result
+        return " ".join(generated)
